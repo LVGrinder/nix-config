3c8b4db957790b65333da2ddbeab99cd037a1aed
Daniel Aanensen <tetochrono@protonmail.com>
2024-12-16 09:15:21 +0100

    fix(system): fixing up config

--- configuration.nix
+++ configuration.nix
@@ -78,40 +78,6 @@
  
   
   
-  home-manager.users.teto = { pkgs, ... }: {
-  home.packages = [
-	pkgs.firefox
-	pkgs.signal-desktop
-  pkgs.ripgrep
-  pkgs.xterm
-  pkgs.webcord
-  pkgs.obs-studio
-  # pkgs.gh
-	];
-	
-
-
-
-	programs.bash.enable = true;
-  programs.git = {
-    enable = true;
-    userName  = "Daniel Aanensen";
-    userEmail = "tetochrono@protonmail.com";
-  };
-  # programs.gh.enable = true;
-  programs.git.extraConfig.github.user = "LVGrinder";
-
-  programs.gh = {
-  enable = true;
-};
-
-	
-
-	
-
-	home.stateVersion = "25.05";
-
-  };
 
 
    fonts = {
