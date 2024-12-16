862343907ba8da24d2d04bde85ba0e4073506c61
Daniel Aanensen <tetochrono@protonmail.com>
2024-12-16 05:03:41 +0100

    feat(nixos): symlink

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 
-sudo ln /etc/nixos/configuration.nix ./configuration.nix
-sudo ln /etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
 
-sudo mkdir ./dotfiles/hypr
-sudo ln ~/.config/hypr/hyprland.conf ./dotfiles/hypr/hyprland.conf
+# Delete first
+sudo rm /etc/nixos/configuration.nix
+sudo rm /etc/nixos/hardware-configuration.nix
+sudo rm -r ~/.config/hypr
+
+
+sudo ln -s ~/nix-config/configuration.nix /etc/nixos/configuration.nix 
+sudo ln -s ~/nix-config/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+
+sudo mkdir ~/.config/hypr
+sudo ln -s ~/nix-config/dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
