81ce44ccce6c2fa912954c1dbfaf0a2cc5537504
Daniel Aanensen <tetochrono@protonmail.com>
2024-12-17 06:43:51 +0100

    feat(nixfmt): installed nix formatter

--- system/neovim.nix
+++ system/neovim.nix
@@ -1,4 +1,3 @@
-
 { config, pkgs, ... }:
 
 {
@@ -9,23 +8,25 @@
 
   };
 
-    # Install LazyVim dependencies
-environment.systemPackages = with pkgs; [
-  neovim          # Neovim itself
-  ripgrep         # Needed for telescope.nvim
-  fd              # Fast file searching
-  fzf             # Fuzzy finder
-  nodejs          # Required for LSP and CoC plugins
-  gcc             # Compiler for plugins like treesitter
-  curl
-  wget
-  unzip           # For extracting plugins
-  git             # Needed for LazyVim and plugin managers
-  python3         # Python support for plugins
-  nil
-  cargo # needed for nix lsp
+  # Install LazyVim dependencies
+  environment.systemPackages = with pkgs; [
+    neovim # Neovim itself
+    ripgrep # Needed for telescope.nvim
+    fd # Fast file searching
+    fzf # Fuzzy finder
+    nodejs # Required for LSP and CoC plugins
+    gcc # Compiler for plugins like treesitter
+    curl
+    wget
+    unzip # For extracting plugins
+    git # Needed for LazyVim and plugin managers
+    python3 # Python support for plugins
+    nil
+    cargo # needed for nix lsp
     git
     clang
-];
+    nil
+    nixfmt-rfc-style
+  ];
   programs.lazygit.enable = true;
 }
