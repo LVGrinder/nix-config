6836ee97cf5f72b20a2961447182f480d7431c4c
Daniel Aanensen <tetochrono@protonmail.com>
2024-12-17 07:59:27 +0100

    fix(symlink): adding startup hypr

--- symlink.sh
+++ symlink.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-
-
 # Delete first
 sudo rm /etc/nixos/configuration.nix
 sudo rm /etc/nixos/hardware-configuration.nix
 sudo rm /etc/nixos/flake.nix
 sudo rm -r ~/.config/hypr
 
-
-sudo ln -s ~/nix-config/configuration.nix /etc/nixos/configuration.nix 
+sudo ln -s ~/nix-config/configuration.nix /etc/nixos/configuration.nix
 sudo ln -s ~/nix-config/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 sudo ln -s ~/nix-config/flake.nix /etc/nixos/flake.nix
 
 sudo mkdir ~/.config/hypr
 sudo ln -s ~/nix-config/dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+sudo ln -s ~/nix-config/dotfiles/hypr/startup.conf ~/.config/hypr/startup.conf
