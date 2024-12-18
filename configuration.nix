e56930c675f1686aa74c0b17ac444106a13c45d7
Daniel Aanensen <tetochrono@protonmail.com>
2024-12-18 07:52:12 +0100

    feat(bluetooth): adding bluetooth, adding GUI for volume and bluetooth

--- configuration.nix
+++ configuration.nix
@@ -73,12 +73,15 @@
 
   programs.hyprland.enable = true;
 
+  hardware.bluetooth.enable = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     kitty
+    blueman
+    pavucontrol
   ];
 
   fonts = {
