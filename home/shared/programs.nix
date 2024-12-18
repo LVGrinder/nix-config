8b17230e9d224608bdb9f5d1e0c71f782ab3aede
Daniel Aanensen <tetochrono@protonmail.com>
2024-12-18 09:32:25 +0100

    feat(programs): added thunar (file manager)

--- home/shared/programs.nix
+++ home/shared/programs.nix
@@ -4,7 +4,14 @@
   home = {
     packages = with pkgs; [
       spotify
-      spicetify-cli
+      spicetify-cli # to customize spotify
+      xfce.thunar # gtk file manager
+      xfce.thunar-archive-plugin # Archive support
+      xfce.thunar-media-tags-plugin # Media tag support
+      xfce.thunar-volman # Volume manager for external drives
+      xfce.tumbler # Thumbnail generator for files
+      dconf # Provides a backend for GTK settings
     ];
   };
+
 }
