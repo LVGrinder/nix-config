8492109a099719dbba8de4ad32714f82888953a9
Daniel Aanensen <tetochrono@protonmail.com>
2024-12-18 09:34:02 +0100

    chore(comment): added comment

--- home/shared/programs.nix
+++ home/shared/programs.nix
@@ -10,7 +10,7 @@
       xfce.thunar-media-tags-plugin # Media tag support
       xfce.thunar-volman # Volume manager for external drives
       xfce.tumbler # Thumbnail generator for files
-      dconf # Provides a backend for GTK settings
+      dconf # Provides a backend for GTK settings | Needed for Thunar and XFCE
     ];
   };
 
