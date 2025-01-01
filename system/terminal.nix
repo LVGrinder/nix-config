b3b00d433a1431eba7dba9f2fb904a0aaa3fb8be
Daniel Aanensen <tetochrono@protonmail.com>
2025-01-01 09:49:32 +0100

    fix(tmux): plugins

--- system/terminal.nix
+++ system/terminal.nix
@@ -9,37 +9,48 @@
       keyMode = "vi";
       plugins = with pkgs; [
         tmuxPlugins.catppuccin
+        tmuxPlugins.cpu
+        tmuxPlugins.battery
       ];
-      extraConfig = "
-# Options to make tmux more pleasant
 
-set -g mouse on
-set -g default-terminal 'tmux-256color'
+      extraConfigBeforePlugins = ''
+                # for catppuccin
+              # ~/.tmux.conf
 
-# Configure the catppuccin plugin
-set -g @catppuccin_flavor 'mocha' # latte, frappe, macchiato, or mocha
-set -g @catppuccin_window_status_style 'rounded' # basic, rounded, slanted, custom, or none
+        # Options to make tmux more pleasant
+        set -g mouse on
+        set -g default-terminal "tmux-256color"
 
-# Load catppuccin
-run ~/.config/tmux/plugins/catppuccin/tmux/catppuccin.tmux
-# For TPM, instead use `run ~/.config/tmux/plugins/tmux/catppuccin.tmux`
+        # Configure the catppuccin plugin
+        set -g @catppuccin_flavor "mocha"
+        set -g @catppuccin_window_status_style "rounded"
 
-# Make the status line pretty and add some modules
-set -g status-right-length 100
-set -g status-left-length 100
-set -g status-left ''
-set -g status-right '#{E:@catppuccin_status_application}'
-set -agF status-right '#{E:@catppuccin_status_cpu}'
-set -ag status-right '#{E:@catppuccin_status_session}'
-set -ag status-right '#{E:@catppuccin_status_uptime}'
-set -agF status-right '#{E:@catppuccin_status_battery}'
+        # Load catppuccin
+        run ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
+        # For TPM, instead use `run ~/.config/tmux/plugins/tmux/catppuccin.tmux`
 
-run ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/cpu.tmux
-run ~/.config/tmux/plugins/tmux-plugins/tmux-battery/battery.tmux
-# Or, if using TPM, just run TPM
-        ";
+        # Make the status line pretty and add some modules
+        set -g status-right-length 100
+        set -g status-left-length 100
+        set -g status-left ""
+        set -g status-right "#{E:@catppuccin_status_application}"
+        set -agF status-right "#{E:@catppuccin_status_cpu}"
+        set -ag status-right "#{E:@catppuccin_status_session}"
+        set -ag status-right "#{E:@catppuccin_status_uptime}"
+        set -agF status-right "#{E:@catppuccin_status_battery}"
 
+        run ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+        run ${pkgs.tmuxPlugins.battery}/share/tmux-plugins/battery/battery.tmux
+        # Or, if using TPM, just run TPM;
+      '';
     };
+
+    # {
+    #   plugin = pkgs.
+    #
+    #
+    # }
+
     fish = {
       enable = true;
     };
