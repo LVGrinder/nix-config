c8794f6f6000c6cc16f858ed636205388745b9c7
Daniel Aanensen <tetochrono@protonmail.com>
2025-01-08 20:08:33 +0100

    feat(nvim): lazyvim

---
+++ dotfiles/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- require("which-key").setup {
+--   window = {
+--     position = "bottom", -- Options: "bottom", "top", "left", "right"
+--   },
+-- }
+
