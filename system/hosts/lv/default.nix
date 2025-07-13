cd02f57517dff72517edf660cac5e84a4896aade
Daniel Aanensen <tetochrono@protonmail.com>
2025-07-13 03:07:39 +0200

    feat(LV): regretting going to my old pc which has nvidia, but it works

--- system/hosts/lv/default.nix
+++ system/hosts/lv/default.nix
@@ -11,4 +11,26 @@
   imports = [
     ./hardware-configuration.nix
   ];
+hardware.graphics.enable = true;
+hardware.nvidia = {
+  modesetting.enable = true;
+  powerManagement = {
+    enable = true;
+    finegrained = false;
+  };
+	open = false;
+	nvidiaSettings = true;
+package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+};
+
+
+environment.systemPackages = [
+    pkgs.nvidia-vaapi-driver
+  ];
+
+    # Load nvidia driver for Xorg and Wayland
+    services.xserver.videoDrivers = ["nvidia"];
+environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
 }
