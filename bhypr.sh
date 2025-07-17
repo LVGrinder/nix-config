4f696ec04a297c8225358e8c7f2d3d79f3b96d7b
Daniel Aanensen <tetochrono@protonmail.com>
2025-07-18 00:06:45 +0200

    fix(hypr): bhypr fixed

--- bhypr.sh
+++ bhypr.sh
@@ -5,6 +5,11 @@ current_user=$(whoami)
 
 echo "User is: $current_user"
 
+# save hostname
+current_host=$(hostname)
+
+echo "Host is: $current_host"
+
 #folder location
 hypr=~/nix-config/dotfiles/hypr/user/$current_user/hyprland.conf
 
@@ -14,6 +19,4 @@ sudo rm -r ~/.config/hypr/hyprland.conf
 echo "deleted"
 sudo mkdir ~/.config/hypr
 echo "source = ~/nix-config/dotfiles/hypr/users/$current_user/hyprland.conf" | sudo tee -a ~/.config/hypr/hyprland.conf
-
-
-
+echo "source = ~/nix-config/dotfiles/hypr/hosts/$current_host/hyprland.conf" | sudo tee -a ~/.config/hypr/hyprland.conf
